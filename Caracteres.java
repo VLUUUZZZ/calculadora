c8338cadf0296f73ad59b358d4dff3e06f37e182
uzzielvictoor <xiroda6729@marchub.com>
2025-05-08 08:44:34 -0600

    MI SEGUNDO PROGRAMA EN JAVA

---
+++ Caracteres.java
@@ -0,0 +1,10 @@
+public class Caracteres {
+    public static void main(String[] args) {
+        char letra = 'a';
+        char simbolo = '$';
+        char NumeroChar =  '5';
+
+        System.out.println("Letra = " + letra);
+        System.out.println("NumeroChar = " + NumeroChar);
+    }
+}
